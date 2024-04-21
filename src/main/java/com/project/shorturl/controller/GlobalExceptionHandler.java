1a28658111e15c1932ee6c0e7d5007f3c2101b1b
Anton <antontokmakov757@gmail.com>
2024-04-21 21:40:53 +0700

    Refactoring GlobalExceptionHandler

--- src/main/java/com/project/shorturl/controller/GlobalExceptionHandler.java
+++ src/main/java/com/project/shorturl/controller/GlobalExceptionHandler.java
@@ -21,38 +21,48 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GeneratedUrlException.class)
-    public ResponseEntity<ExceptionResponse> handleGeneratedUrlException(HttpServletRequest request, Exception exception) {
-        return getResponse(request, exception);
+    public ResponseEntity<ExceptionResponse> handleGeneratedUrlException(HttpServletRequest request,
+                                                                         Exception exception) {
+        return getResponse(request, exception, BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleProcessValidationError(HttpServletRequest request, Exception exception, BindingResult bindingResult) {
-        return getResponseEntityValid(request, exception, bindingResult);
+    public ResponseEntity<ExceptionResponse> handleProcessValidationError(HttpServletRequest request,
+                                                                          Exception exception,
+                                                                          BindingResult bindingResult) {
+        return getResponseEntityValid(request, exception, bindingResult, BAD_REQUEST);
     }
 
     @ExceptionHandler(RedirectException.class)
-    public ResponseEntity<ExceptionResponse> handleRedirectException(HttpServletRequest request, Exception exception) {
-        return getResponse(request, exception);
+    public ResponseEntity<ExceptionResponse> handleRedirectException(HttpServletRequest request,
+                                                                     Exception exception) {
+        return getResponse(request, exception, HttpStatus.GONE);
     }
 
     @ExceptionHandler(LifeTimeExpiredException.class)
-    public ResponseEntity<ExceptionResponse> handleLifeTimeExpiredException(HttpServletRequest request, Exception exception) {
-        return getResponse(request, exception);
+    public ResponseEntity<ExceptionResponse> handleLifeTimeExpiredException(HttpServletRequest request,
+                                                                            Exception exception) {
+        return getResponse(request, exception, HttpStatus.NOT_FOUND);
     }
 
 
-    private ResponseEntity<ExceptionResponse> getResponseEntityValid(HttpServletRequest request, Exception exception, BindingResult bindingResult) {
+    private ResponseEntity<ExceptionResponse> getResponseEntityValid(HttpServletRequest request,
+                                                                     Exception exception,
+                                                                     BindingResult bindingResult,
+                                                                     HttpStatus status) {
         log.warn(exception.getClass().getName() + ": " + exception.getMessage());
         String message = String.valueOf(bindingResult.getAllErrors()
                 .stream()
                 .map(ObjectError::getDefaultMessage)
                 .toList());
-        return ResponseEntity.status(BAD_REQUEST).body(new ExceptionResponse(request.getRequestURI(), message, HttpStatus.BAD_REQUEST));
+        return ResponseEntity.status(status)
+                .body(new ExceptionResponse(request.getRequestURI(), message));
     }
 
-    private ResponseEntity<ExceptionResponse> getResponse(HttpServletRequest request, Exception exception) {
+    private ResponseEntity<ExceptionResponse> getResponse(HttpServletRequest request, Exception exception, HttpStatus status) {
         log.warn(exception.getClass().getName() + ": " + exception.getMessage());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(request.getRequestURI(), exception.getMessage(), HttpStatus.BAD_REQUEST));
+        return ResponseEntity.status(status)
+                .body(new ExceptionResponse(request.getRequestURI(), exception.getMessage()));
     }
 
 }
--- src/main/java/com/project/shorturl/controller/dto/ExceptionResponse.java
+++ src/main/java/com/project/shorturl/controller/dto/ExceptionResponse.java
@@ -1,6 +1,4 @@
 package com.project.shorturl.controller.dto;
 
-import org.springframework.http.HttpStatus;
-
-public record ExceptionResponse(String uri, String message, HttpStatus status) {
+public record ExceptionResponse(String uri, String message) {
 }
